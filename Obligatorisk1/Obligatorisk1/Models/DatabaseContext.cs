bfbee6ab6720af1e77459a98410a0dc163adc46b
Nikolaj Holden <nikolajholden@gmail.com>
2015-11-02 16:03:07 +0100

    Added dbcontext

---
+++ Obligatorisk1/Obligatorisk1/Models/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using LabModel.Models;
+
+namespace Obligatorisk1.Models
+{
+    public class DatabaseContext:DbContext
+    {
+        public IDbSet<Component> Components { get; set; }
+        public IDbSet<LoanInformation> LoanInformations { get; set; }
+        public IDbSet<User> Users { get; set; }
+        public IDbSet<SpecificComponent> SpecificComponents { get; set; }
+    }
+}
