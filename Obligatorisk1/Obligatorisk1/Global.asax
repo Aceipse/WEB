75eba47f28db119259082f09af38af6c2dcd2179
Nikolaj Holden <nikolajholden@gmail.com>
2015-11-02 15:13:04 +0100

    Initial commit

---
+++ Obligatorisk1/Obligatorisk1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Obligatorisk1.MvcApplication" Language="C#" %>
---
+++ Obligatorisk1/Obligatorisk1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Obligatorisk1.Startup))]
+namespace Obligatorisk1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
