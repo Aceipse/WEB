f50ce5daeb093742c20ee3b6e82b1bb849d7f41b
aceipse <martin.fig@hotmail.com>
2015-11-02 15:40:46 +0100

    Added LoanInformation and User

---
+++ Obligatorisk1/Obligatorisk1/Models/LoanInformation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabModel.Models
+{
+    public class LoanInformation
+    {
+        public int Id { get; set; }
+        public Component Component { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public string IsEmailSend { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public User User { get; set; }
+        public string ReservationId { get; set; }
+    }
+}
---
+++ Obligatorisk1/Obligatorisk1/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabModel.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string StudieNumber { get; set; }
+        public string Name { get; set; }
+        public string MobileNumber { get; set; }
+    }
+}
