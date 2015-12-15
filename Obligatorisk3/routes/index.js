8976e7939781f79800b4e79280947e4983c485d1
Nikolaj Holden <nikolajholden@gmail.com>
2015-12-15 14:15:54 +0100

    Changed folder structure

--- Obligatorisk3/routes/index.js
+++ Obligatorisk3/routes/index.js
@@ -2,50 +2,15 @@ var express = require('express');
 var mongoose= require('mongoose');
 var router = express.Router();
 
-mongoose.connect('mongodb://aaa:123@ds042128.mongolab.com:42128/MongoLab-qq')
-var db=mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-
-  var fitnessUserSchema= mongoose.Schema({
-    name: String,
-    log:  Array
-  });
-  var workoutProgramSchema=mongoose.Schema({
-    name: String,
-    exercises:  Array
-  });
-  var exerciseSchema=mongoose.Schema({
-    name: String,
-    description: String,
-    numberOfSets: Number,
-    repetitionsOrTime: String
-  });
-  
-  
-  var fitnessUser= mongoose.model('fitnessUser',fitnessUserSchema);
-  var workoutProgram= mongoose.model('workoutProgram',workoutProgramSchema);
-  var exercise= mongoose.model('exercise',exerciseSchema);
-/*  
-  var nikolaj=new fitnessUser({name:'Nikolaj',log:[]})
+var homeController=function(req,res){
+    var fitnessUser=mongoose.model('fitnessUser');
+    var nikolaj=new fitnessUser({name:'aaaaaaj',log:[]})
   nikolaj.save(function (err, fluffy) {
   if (err) return console.error(err);
 });
-
-var exercisea=new exercise({name:'squat',description:'very leg exercise',numberOfSets:100,repetitionsOrTime:'500 reps Brah'})
-
-
-var woprogram=new workoutProgram({name:'Awesome workout',exercises:[exercisea]})
-  woprogram.save(function (err, fluffy) {
-  if (err) return console.error(err);
-});
-*/
-
-
-});
+    res.render('index', { title: 'Express' });
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', homeController);
 
 module.exports = router;
