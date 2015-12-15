8d19fb122eed12232e5487a42922d23f0297af94
Nikolaj Holden <nikolajholden@gmail.com>
2015-12-15 13:43:25 +0100

    Added mongoose and mongodb connection

--- Obligatorisk3/routes/index.js
+++ Obligatorisk3/routes/index.js
@@ -1,6 +1,48 @@
 var express = require('express');
+var mongoose= require('mongoose');
 var router = express.Router();
 
+mongoose.connect('mongodb://aaa:123@ds042128.mongolab.com:42128/MongoLab-qq')
+var db=mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+
+  var fitnessUserSchema= mongoose.Schema({
+    name: String,
+    log:  Array
+  });
+  var workoutProgramSchema=mongoose.Schema({
+    name: String,
+    exercises:  Array
+  });
+  var exerciseSchema=mongoose.Schema({
+    name: String,
+    description: String,
+    numberOfSets: Number,
+    repetitionsOrTime: String
+  });
+  
+  
+  var fitnessUser= mongoose.model('fitnessUser',fitnessUserSchema);
+  var workoutProgram= mongoose.model('workoutProgram',workoutProgramSchema);
+  var exercise= mongoose.model('exercise',exerciseSchema);
+/*  
+  var nikolaj=new fitnessUser({name:'Nikolaj',log:[]})
+  nikolaj.save(function (err, fluffy) {
+  if (err) return console.error(err);
+});
+
+var exercisea=new exercise({name:'squat',description:'very leg exercise',numberOfSets:100,repetitionsOrTime:'500 reps Brah'})
+
+
+var woprogram=new workoutProgram({name:'Awesome workout',exercises:[exercisea]})
+  woprogram.save(function (err, fluffy) {
+  if (err) return console.error(err);
+});
+*/
+
+
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
